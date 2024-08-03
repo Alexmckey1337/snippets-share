4e9bd03120bcb8c20256acf779bc2a599e3941f7
Alex Yakubin <wallahack35@gmail.com>
2024-08-03 12:14:56 +0300

    feat: implemented add server function

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,25 @@
-export default function CreateSnippet() {
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    "use server";
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    console.log(snippet);
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bald m-3">Create a snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
